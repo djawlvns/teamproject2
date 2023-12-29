71a8658aad8ca518982c78e4a636bac31c1620e0
djawlvns <79679442+djawlvns@users.noreply.github.com>
2023-12-29 12:04:41 +0900

    Add files via upload

---
+++ dws/src/main/java/com/tp/dws/service/UserService.java
@@ -0,0 +1,11 @@
+package com.tp.dws.service;
+
+import com.tp.dws.dto.BaseResponse;
+import com.tp.dws.dto.UserDto;
+import com.tp.dws.dto.UserLoginDto;
+
+public interface UserService {
+
+	public BaseResponse<Void> signUp(UserDto memberdto);
+	public BaseResponse<Void> login(UserLoginDto memberLoginDto);
+}
